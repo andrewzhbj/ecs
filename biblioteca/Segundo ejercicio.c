477319f35da9f9863b65e47b58011ad6a48f417b
Manuel Cabral <nani.andres@hotmail.com.ar>
2020-06-19 14:00:28 -0300

    Update Segundo ejercicio.c

--- biblioteca/Segundo ejercicio.c
+++ biblioteca/Segundo ejercicio.c
@@ -1,11 +1,9 @@
 #include "BasicaLib.h"
-#define True 	1
-#define False 	0
-// Calcular  y mostrar el cuadrado de un número mayor a 0
+// Calcular  y mostrar el cuadrado de un nÃºmero mayor a 0
 
 int main(){
 	int Numero = IngresarNumero();
-	if(MayorACero(Numero) == True)
+	if(MayorACero(Numero))
 		MostrarDato(AlCuadrado(Numero));	
 	else 
 		MostrarTexto("Error: el numero debe ser mayor a 0");
